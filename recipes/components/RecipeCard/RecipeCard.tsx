7ae879efb224d83322ea7a5071b556789a5af65c
Omar Ebrahim <o_ebrahim1@outlook.com>
2023-11-04 06:14:59 +0000

    Made card look nice

--- recipes/components/RecipeCard/RecipeCard.tsx
+++ recipes/components/RecipeCard/RecipeCard.tsx
@@ -18,17 +18,17 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
     tags,
   } = recipe;
   return (
-    <div className="border shadow-md w-[250px] h-300px rounded-3xl p-4 text-center">
+    <div className="border shadow-md w-[250px] h-300px rounded-3xl text-center dark:bg-gray-700 bg-slate-100  dark:text-gray-300 text-slate-600 pb-4">
       <Image
         src={featuredImageUrl}
         alt={featuredImageAlt}
-        width={220}
+        width={250}
         height={150}
-        className="rounded-2xl"
+        className="rounded-t-2xl"
       />
-      <p className="text-slate-600 pt-2 font-semibold text-xl">{title}</p>
-      {subtitle && <small>{subtitle}</small>}
-      <p className="inline-flex gap-4 text-center align-middle"><Clock className="w-6 h-6" /><span>{prepTime+cookTime} mins</span></p>
+      <p className="pt-2 pl-4 pr-4 font-semibold text-xl">{title}</p>
+      {subtitle && <small className="inline-block p-2 text-[1em]">{subtitle}</small>}
+      <p className="inline-flex gap-4 text-center align-middle"><Clock className="w-6 h-6" /><span>{prepTime + cookTime} mins</span></p>
     </div>
   );
 };
