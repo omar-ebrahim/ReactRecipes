55297324a97975e32be0fa2ff4815b25156940f0
Omar Ebrahim <o_ebrahim1@outlook.com>
2023-11-07 16:30:29 +0000

    Mobile and desktop layouts corrected

--- recipes/app/(frontend)/[slug]/components/RecipeView.tsx
+++ recipes/app/(frontend)/[slug]/components/RecipeView.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { RecipeViewRecipe, RecipeViewRecipeIngredient } from "@/sanity/queries/getReipceBySlug";
-import css from './recipeView.module.css'
 import Image from "next/image";
 import { PortableText } from '@portabletext/react';
+import css from './recipeView.module.css'
 
 type RecipeViewProps = {
     data: RecipeViewRecipe;
@@ -25,26 +25,25 @@ const RecipeView = ({ data: recipe }: RecipeViewProps) => {
         ingredients,
         instructions,
         prepTime,
-        slug,
         title,
         preamble,
         subtitle,
     } = recipe;
     console.log(recipe)
     return (
-        <div className="p-4">
-            <section className="pt-4 pb-8 text-center">
+        <div className="p-4 lg:flex-col lg:gap-x-20 lg:grid-cols-[minmax(700px,_900px)_minmax(200px,_1fr)] lg:grid lg:w-fit lg:mx-auto lg:grid-rows-[auto_1fr]">
+            <section className="pt-4 pb-8 text-center content-center">
                 <header className="font-bold text-4xl pb-4">{title}</header>
                 {subtitle && <h2 className="pt-4 pb-6 italic">{subtitle}</h2>}
-                {featuredImageUrl && <Image src={featuredImageUrl} alt={featuredImageAlt} width={100} height={100} className="w-full max-h-[300px] object-cover rounded-md" />}
+                {featuredImageUrl && <Image src={featuredImageUrl} alt={featuredImageAlt} width={100} height={100} className="w-full max-h-[300px] lg:w-[200px] lg:rounded-full lg:h-[200px] lg:object-fill object-cover rounded-md lg:mx-auto" />}
             </section>
-            <section className="pb-8">
+            <section className="pb-8 lg:pt-36">
                 <p>Prep time: {prepTime} mins.</p>
                 <p>Cook time: {cookTime} mins.</p>
                 {ingredients && (
                     <section id="ingredients" className="pt-8">
-                        <h1 className="font-semibold text-3xl">Ingredients</h1>
-                        <div className="mb-2 mt-4 underline text-slate-600">
+                        <h1 className="font-semibold text-3xl lg:text-xl">Ingredients</h1>
+                        <div className="mb-2 mt-4 underline text-slate-600 lg:hidden">
                             <a href='#recipe'>Jump to instructions</a>
                         </div>
                         <ul className="list-disc pt-2">
@@ -55,21 +54,21 @@ const RecipeView = ({ data: recipe }: RecipeViewProps) => {
                     </section>
                 )}
             </section>
-            <div className={css.preambleAndInstructions}>
+            <div>
                 {preamble && (
                     <>
-                        <header className="font-semibold text-3xl pb-2">Introduction</header>
-                        <div className="mb-4 underline text-slate-600">
+                        <header className="font-semibold text-3xl pb-2 lg:pb-4">Introduction</header>
+                        <div className="mb-4 underline text-slate-600 lg:hidden">
                             <a href='#recipe'>Jump to instructions</a>
                         </div>
                         <PortableText value={preamble} />
                     </>
                 )}
                 <article>
-                    <header id="recipe" className="font-semibold text-3xl pb-2 mt-4">Recipe</header>
-                    <div className="mb-4 underline text-slate-600">
-                            <a href='#ingredients'>Jump to ingredients</a>
-                        </div>
+                    <header id="recipe" className="font-semibold text-3xl pb-2 mt-4 lg:pb-4">Recipe</header>
+                    <div className="mb-4 underline text-slate-600 lg:hidden">
+                        <a href='#ingredients'>Jump to ingredients</a>
+                    </div>
                     <PortableText value={instructions} />
                 </article>
             </div>
