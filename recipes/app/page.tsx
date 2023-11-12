b9da73fe7904b289097ac15ed91ea78284218a63
Omar Ebrahim <o_ebrahim1@outlook.com>
2023-11-12 15:30:05 +0000

    Added text search

--- recipes/app/page.tsx
+++ recipes/app/page.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import RecipeCard from "@/components/recipecard/RecipeCard";
-import { getAllRecipes } from "@/sanity/queries/getAllRecipes";
+import { HomepageRecipe, getAllRecipes } from "@/sanity/queries/getAllRecipes";
+import { useState } from "react";
 
 export default async function Home() {
-  const recipes = await getAllRecipes();
+  const [recipes, setRecipes] = useState<HomepageRecipe[]>([]);
   return (
-    <div className="gap-4 p-4 mx-auto h-full md:grid lg:grid lg:grid-cols-4 md:grid-cols-2 w-fit">
-      {recipes.map((recipe) => (
-        <RecipeCard key={`$recipe_card_${recipe._id}`} recipe={recipe} />
-      ))}
-    </div>
+    <>
+      <div className="w-full px-4">
+        <input
+          type="text"
+          className="w-full md:w-[400px] md:h-16 md:text-xl border border-slate-200 drop-shadow-md rounded-[24px] text-center block mx-auto p-2 mt-6"
+          placeholder="Search for a recipe"
+        />
+      </div>
+      <div className="gap-4 p-4 mx-auto h-full md:grid lg:grid lg:grid-cols-4 md:grid-cols-2 w-fit">
+        {recipes.map((recipe) => (
+          <RecipeCard key={`$recipe_card_${recipe._id}`} recipe={recipe} />
+        ))}
+      </div>
+    </>
   );
 }
