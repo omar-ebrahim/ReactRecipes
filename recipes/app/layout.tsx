aa0a76d38288aab5737176b24c06398d3df062e7
Omar Ebrahim <o_ebrahim1@outlook.com>
2023-11-16 11:41:10 +0000

    Added header image

--- recipes/app/layout.tsx
+++ recipes/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../tailwind.css'
 import Navbar from '../../recipes/components/navbar/navbar'
-import SearchBar from '@/components/searchbar/SearchBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <SearchBar />
         {children}
       </body>
     </html>
--- recipes/app/page.tsx
+++ recipes/app/page.tsx
@@ -1,13 +1,31 @@
 import RecipeCard from "@/components/recipecard/RecipeCard";
 import { getAllRecipes } from "@/sanity/queries/getAllRecipes";
+import Pizza from "../images/pizza.jpg";
+import Image from "next/image";
+import SearchBar from "@/components/searchbar/SearchBar";
 
 export default async function Home() {
   const recipes = await getAllRecipes();
   return (
-    <div className="gap-4 p-4 mx-auto h-full md:grid lg:grid lg:grid-cols-4 md:grid-cols-2 w-fit">
-      {recipes.map((recipe) => (
-        <RecipeCard key={`$recipe_card_${recipe._id}`} recipe={recipe} />
-      ))}
+    <div>
+      <div className="relative">
+        <div className="absolute z-30 w-full h-full">
+          <SearchBar />
+        </div>
+        <Image
+          src={Pizza.src}
+          width={1920}
+          height={1080}
+          alt="pizza"
+          className="w-full h-[180px] md:h-[350px] object-cover object-center opacity-75"
+        />
+      </div>
+
+      <div className="gap-4 p-4 mx-auto h-full md:grid lg:grid lg:grid-cols-4 md:grid-cols-2 w-fit">
+        {recipes.map((recipe) => (
+          <RecipeCard key={`$recipe_card_${recipe._id}`} recipe={recipe} />
+        ))}
+      </div>
     </div>
   );
 }
