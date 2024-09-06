2ae5dc5d631d3e58512d30f11df3e8cfdb4bcff4
Omar Ebrahim <o_ebrahim1@outlook.com>
2024-09-06 17:37:10 +0100

    Replaced CSS for RecipeSidebar

--- recipes/components/recipesidebar/RecipeSidebar.tsx
+++ recipes/components/recipesidebar/RecipeSidebar.tsx
@@ -1,5 +1,7 @@
 import { RecipeViewRecipeIngredient } from "@/sanity/queries/getRecipeBySlug";
 
+import styles from './RecipeSidebar.module.css';
+
 type RecipeSidebarProps = {
     prepTime: number;
     cookTime: number;
@@ -15,23 +17,25 @@ const formatIngredient = (ingredient: RecipeViewRecipeIngredient) => {
 }
 
 const RecipeSidebar = ({ prepTime, cookTime, ingredients }: RecipeSidebarProps) => {
-    return (<section className="pb-8 lg:pt-36">
-        <p>Prep time: {prepTime} mins.</p>
-        <p>Cook time: {cookTime} mins.</p>
-        {ingredients && (
-            <section id="ingredients" className="pt-8">
-                <h1 className="font-semibold text-3xl lg:text-xl">Ingredients</h1>
-                <div className="mb-2 mt-4 underline text-slate-600 lg:hidden">
-                    <a href='#recipe'>Jump to instructions</a>
-                </div>
-                <ul className="list-disc pt-2">
-                    {ingredients.map((ingredient) => {
-                        return <li key={`ingredient_item_${ingredient._id}`} className="ml-5 pb-1 pt-1">{formatIngredient(ingredient)}</li>
-                    })}
-                </ul>
-            </section>
-        )}
-    </section>)
+    return (
+        <section className={styles.sidebarSection}>
+            <p>Prep time: {prepTime} mins.</p>
+            <p>Cook time: {cookTime} mins.</p>
+            {ingredients && (
+                <section id="ingredients" className={styles.ingredients}>
+                    <h1>Ingredients</h1>
+                    <div className={styles.jumpLink}>
+                        <a href='#recipe'>Jump to instructions</a>
+                    </div>
+                    <ul className={styles.ingredientsList}>
+                        {ingredients.map((ingredient) => {
+                            return <li key={`ingredient_item_${ingredient._id}`} className={styles.ingredientItem}>{formatIngredient(ingredient)}</li>
+                        })}
+                    </ul>
+                </section>
+            )}
+        </section>
+    )
 };
 
 export default RecipeSidebar;
