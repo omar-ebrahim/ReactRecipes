ca689f3840965d6257314aaf76abdd194b920cd5
Omar Ebrahim <o_ebrahim1@outlook.com>
2024-09-06 13:35:07 +0100

    Removed tailwind from the RecipeView

--- recipes/app/(frontend)/[slug]/components/RecipeView.tsx
+++ recipes/app/(frontend)/[slug]/components/RecipeView.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { RecipeViewRecipe, RecipeViewRecipeIngredient } from "@/sanity/queries/getRecipeBySlug";
+import { RecipeViewRecipe } from "@/sanity/queries/getRecipeBySlug";
 import RecipeHeader from "@/components/recipeheader/RecipeHeader";
 import RecipeSidebar from "@/components/recipesidebar/RecipeSidebar";
 import RecipePreamble from "@/components/recipepreamble/RecipePreamble";
 import RecipeDetails from "@/components/recipedetails/RecipeDetails";
+
+import styles from './RecipeView.module.css'
 
 type RecipeViewProps = {
     data: RecipeViewRecipe;
@@ -23,7 +25,7 @@ const RecipeView = ({ data: recipe }: RecipeViewProps) => {
         subtitle,
     } = recipe;
     return (
-        <div className="p-4 lg:flex-col lg:gap-x-20 lg:grid-cols-[minmax(700px,_900px)_minmax(200px,_1fr)] lg:grid lg:w-fit lg:mx-auto lg:grid-rows-[auto_1fr]">
+        <div className={styles.mainContainer}>
             <RecipeHeader title={title} featuredImageAlt={featuredImageAlt} featuredImageUrl={featuredImageUrl} subtitle={subtitle} />
             <RecipeSidebar cookTime={cookTime} ingredients={ingredients} prepTime={prepTime} />
             <div>
