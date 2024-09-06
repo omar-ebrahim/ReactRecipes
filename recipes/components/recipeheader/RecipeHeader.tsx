46863d9111412e930899ca10908e667ab08187ab
Omar Ebrahim <o_ebrahim1@outlook.com>
2024-09-06 17:23:18 +0100

    Udpated CSS for RecipeHeader

--- recipes/components/recipeheader/RecipeHeader.tsx
+++ recipes/components/recipeheader/RecipeHeader.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from './RecipeHeader.module.css';
 
 type RecipeHeaderProps = {
     title: string;
@@ -9,10 +10,10 @@ type RecipeHeaderProps = {
 
 const RecipeHeader = ({ title, subtitle, featuredImageAlt = '', featuredImageUrl }: RecipeHeaderProps) => {
     return (
-        <section className="pt-4 pb-8 text-center content-center">
-            <header className="font-bold text-4xl pb-4">{title}</header>
-            {subtitle && <h2 className="pt-4 pb-6 italic">{subtitle}</h2>}
-            {featuredImageUrl && <Image priority src={featuredImageUrl} alt={featuredImageAlt} width={100} height={100} className="w-full max-h-[300px] lg:w-[250px] lg:rounded-full lg:h-[250px] object-cover rounded-md lg:mx-auto" />}
+        <section className={styles.sectionContainer}>
+            <header>{title}</header>
+            {subtitle && <h2>{subtitle}</h2>}
+            {featuredImageUrl && <Image priority src={featuredImageUrl} alt={featuredImageAlt} width={100} height={100} className={styles.featuredImage} />}
         </section>
     )
 };
