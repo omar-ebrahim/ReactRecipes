597a7b8ca640755326aec6ae10dff44b7531b18b
Omar Ebrahim <o_ebrahim1@outlook.com>
2024-09-06 07:44:26 +0100

    Updated css for recipes section

--- recipes/app/page.tsx
+++ recipes/app/page.tsx
@@ -1,26 +1,13 @@
-import RecipeCard from "@/components/recipecard/RecipeCard";
+import RecipesSection from "@/components/recipessection/RecipesSection";
 import SearchHeader from "@/components/searchheader/SearchHeader";
 import { getRecipesByAllTags } from "@/sanity/queries/getRecipesByTags";
-import Link from "next/link";
 
 export default async function Home() {
   const recipes = await getRecipesByAllTags();
   return (
-    <div>
+    <>
       <SearchHeader />
-      {recipes.map((recipeGroup) => (
-        <section key={`section_group_${recipeGroup._id}`} className="pb-6 md:max-w-[1200px] mx-auto">
-          <header className="pt-4 md:pt-8 px-8 mx-auto text-4xl text-center text-slate-700">
-            <Link className="font-extrabold" href={`/recipesbytype/${recipeGroup.slug}`}>{recipeGroup.name}</Link>
-          </header>
-          <div className="px-4 gap-4 h-full md:grid lg:grid lg:grid-cols-4 md:grid-cols-2 w-full">
-            {recipeGroup.recipes.map((recipe) => (
-              <RecipeCard key={`$recipe_card_${recipe._id}`} recipe={recipe} />
-            ))}
-          </div>
-          <div className="mx-auto mt-8 w-[200px] border border-b-slate-50"></div>
-        </section>
-      ))}
-    </div>
+      <RecipesSection recipes={recipes} />
+    </>
   );
 }
---
+++ recipes/components/recipessection/RecipesSection.tsx
@@ -0,0 +1,29 @@
+import { RecipeByTag } from "@/sanity/queries/getRecipesByTags"
+import Link from "next/link";
+import RecipeCard from "../recipecard/RecipeCard";
+
+import styles from './RecipesSection.module.css'
+
+type RecipesSectionProps = {
+    recipes: RecipeByTag[];
+}
+
+const RecipesSection = ({ recipes }: RecipesSectionProps) => {
+    return (
+        recipes.map((recipeGroup) => (
+            <section key={`section_group_${recipeGroup._id}`} className={styles.sectionGroup}>
+                <header className={styles.sectionHeader}>
+                    <Link className={styles.title} href={`/recipesbytype/${recipeGroup.slug}`}>{recipeGroup.name}</Link>
+                </header>
+                <div className={styles.cardGroupContainer}>
+                    {recipeGroup.recipes.map((recipe) => (
+                        <RecipeCard key={`$recipe_card_${recipe._id}`} recipe={recipe} />
+                    ))}
+                </div>
+                <div className={styles.sectionDivider}></div>
+            </section>
+        ))
+    )
+};
+
+export default RecipesSection;
