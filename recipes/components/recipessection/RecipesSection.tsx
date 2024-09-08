60cdfacdd228fdc001e29b921b69a5a03be05e71
Omar Ebrahim <o_ebrahim1@outlook.com>
2024-09-08 08:29:35 +0100

    Made the recipes headers more obvious that they can be clicked on

--- recipes/components/recipessection/RecipesSection.tsx
+++ recipes/components/recipessection/RecipesSection.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import RecipeCard from "../recipecard/RecipeCard";
 
 import styles from './RecipesSection.module.css'
-import SectionDivider from "../sectiondivider/SectionDivider";
 
 type RecipesSectionProps = {
     recipes: RecipeByTag[];
@@ -14,14 +13,15 @@ const RecipesSection = ({ recipes }: RecipesSectionProps) => {
         recipes.map((recipeGroup) => (
             <section key={`section_group_${recipeGroup._id}`} className={styles.sectionGroup}>
                 <header className={styles.sectionHeader}>
-                    <Link className={styles.title} href={`/recipesbytype/${recipeGroup.slug}`}>{recipeGroup.name}</Link>
+                    <div className={styles.titleWrapper}>
+                        <Link className={styles.title} href={`/recipesbytype/${recipeGroup.slug}`}>{recipeGroup.name}</Link>
+                    </div>
                 </header>
                 <div className={styles.cardGroupContainer}>
                     {recipeGroup.recipes.map((recipe) => (
                         <RecipeCard key={`$recipe_card_${recipe._id}`} recipe={recipe} />
                     ))}
                 </div>
-                <SectionDivider />
             </section>
         ))
     )
